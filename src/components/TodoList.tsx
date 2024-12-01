279aeea46e832830c1c1075d720128d34bf20efb
Gulnur <gulnurturdumamatova84@gmail.com>
2024-12-01 18:07:27 +0600

    image

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,9 +2,10 @@
 import { FC } from "react";
 import scss from "./TodoList.module.scss";
 import { useDeleteTodoMutation, useGetTodosQuery } from "@/redux/api/todo";
+import Image from "next/image";
 
 const TodoList: FC = () => {
-  const { data, isLoading } = useGetTodosQuery();
+  const { data } = useGetTodosQuery();
   const [deleteTodoMutation] = useDeleteTodoMutation();
   return (
     <section className={scss.TodoList}>
@@ -13,7 +14,9 @@ const TodoList: FC = () => {
           {data?.map((el) => (
             <div key={el._id} className="">
               <h1>{el.title}</h1>
-              <img src={el.img} alt="image" />
+              {el.img && (
+                <Image src={el.img} alt="image" width={300} height={300} />
+              )}
               <button onClick={() => deleteTodoMutation(el._id!)}>
                 delete
               </button>
